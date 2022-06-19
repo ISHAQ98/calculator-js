87bba4d52756c157b021c38eda4a9c67b11065ce
ISHAQ <68194895+ISHAQ98@users.noreply.github.com>
2022-06-19 17:30:27 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 (function () {
+  // Grap All the html elements
   let screen = document.querySelector(".screen");
   let buttons = document.querySelectorAll(".btn");
   let clear = document.querySelector(".btn-red");
   let equal = document.querySelector(".btn-blue");
 
+  // function for buttons to be clicked
+  
   buttons.forEach(function (button) {
     button.addEventListener("click", function (e) {
       let value = e.target.dataset.num;
@@ -11,6 +14,8 @@
     });
   });
 
+  // function calculator's screen
+  
   equal.addEventListener("click", function (e) {
     if (screen.value == " ") {
       screen.value = "Please enter";
